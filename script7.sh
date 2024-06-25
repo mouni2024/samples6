df05a01de712361e544dff03e72e3a638d75b059
mouni2024 <mounikaneelam2002@gmail.com>
2024-06-25 10:14:16 +0530

    Add files via upload

---
+++ script7.sh
@@ -0,0 +1,5 @@
+echo "The number of argumrnts sent to the shell script is $#"
+echo "The number of the script being excuted is $0"
+echo "First argument sent to the script is $1"
+echo "Second argument sent to the script is $2"
+
